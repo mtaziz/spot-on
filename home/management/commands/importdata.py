27c04d04c610c71ac1bc007bfa310bbda7d5c855
nctl144 <nctl144@gmail.com>
2018-03-21 13:49:46 -0400

    create import command

---
+++ home/management/commands/importdata.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand, CommandError
+from home.models import Product
+
+import json
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('data', nargs=1)
+
+    def handle(self, *args, **options):
+        file_name = options['data'][0]
+
+        with open(file_name, 'r') as json_data:
+            for product in json.load(json_data):
+                product_detail = Product(product_name=str(product['product_name']),
+                       product_price=str(product['product_price']),
+                       product_image_url=str(product['product_image_url']),
+                       product_stock=str(product['product_stock']),
+                       product_rating=str(product['product_rating']),
+                       product_url=str(product['product_url']),
+                       product_subtitle=str(product['product_subtitle'])
+                       )
+
+                product_detail.save()
+
+                self.stdout.write(self.style.SUCCESS('Successfully import data'))
