21abe224e875248e889c165171c9844501e25a0f
nctl144 <nctl144@gmail.com>
2018-03-21 10:34:05 -0400

    view all the detail about the product

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -9,8 +9,20 @@ jQuery(function($) {
             var product_url = field.product_url;
 
             $('.mdl-grid.result-area').append('\
-                <div>\
-                    <h1>' + product_name + '</h1>\
+                <div class="mdl-cell mdl-cell--3-col">\
+                    <div class="demo-card-square mdl-card mdl-shadow--2dp">\
+                        <div class="mdl-card__title mdl-card--expand">\
+                            <h2 class="mdl-card__title-text">' + product_name + '</h2>\
+                        </div>\
+                        <div class="mdl-card__supporting-text">\
+                            ' + product_subtitle + '\
+                        </div>\
+                        <div class="mdl-card__actions mdl-card--border">\
+                            <a target="_blank" href="' + product_url + '" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">\
+                                View product\
+                            </a>\
+                        </div>\
+                    </div>\
                 </div>\
             ');
         });
