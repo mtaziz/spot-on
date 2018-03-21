ff1730c18b948054772bacef4637e2a4f25684aa
nctl144 <nctl144@gmail.com>
2018-03-21 10:55:44 -0400

    show images of the product

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -7,15 +7,17 @@ jQuery(function($) {
             var product_stock = field.product_stock;
             var product_rating = field.product_rating;
             var product_url = field.product_url;
+            var product_image_url = field.product_image_url
 
             $('.mdl-grid.result-area').append('\
                 <div class="mdl-cell mdl-cell--3-col">\
                     <div class="demo-card-square mdl-card mdl-shadow--2dp">\
                         <div class="mdl-card__title mdl-card--expand">\
-                            <h2 class="mdl-card__title-text">' + product_name + '</h2>\
+                            <img src="' + product_image_url + '">\
                         </div>\
                         <div class="mdl-card__supporting-text">\
-                            ' + product_subtitle + '\
+                            <h2>' + product_name + '</h2>\
+                            <p>' + product_subtitle + '</p>\
                         </div>\
                         <div class="mdl-card__actions mdl-card--border">\
                             <a target="_blank" href="' + product_url + '" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">\
