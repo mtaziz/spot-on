cf161fcbe658dac3ba6df30ae35d3a2406a9ac43
nctl144 <nctl144@gmail.com>
2018-04-10 16:41:23 -0400

    add placeholder, id to the search box

--- home/forms.py
+++ home/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class SearchForm(forms.Form):
     keywords = forms.CharField(label='keywords',
                                max_length=100,
-                               widget=forms.TextInput(attrs={'data-role':'tagsinput'}))
+                               widget=forms.TextInput(attrs={'data-role':'tagsinput', 'placeholder': 'What\'s on your mind today' }))
