3ac122015c23665104eaf36a2003517d22687253
nctl144 <nctl144@gmail.com>
2018-04-26 15:40:46 -0400

    temp solution for footer appearance

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -1,12 +1,25 @@
 jQuery(function($) {
-    AOS.init();
+    function AOSinit() {
+        AOS.init();
+    }
 
-    if (!$(".mdl-grid.result-area").length || $('.mdl-grid.result-area:empty')) {
-        $('.mdl-mini-footer').css({
-            'position': 'absolute',
-            'bottom': '0',
-            'left': '0',
-            'right': '0'
+    function footerReposition() {
+        if (!$(".mdl-grid.result-area").length || $('.mdl-grid.result-area:empty')) {
+            $('.mdl-mini-footer').css({
+                'position': 'absolute',
+                'bottom': '0',
+                'left': '0',
+                'right': '0'
+            });
+        }
+
+        $(".mdl-mini-footer").css({
+            'opacity': '1',
         });
     }
+
+    $(document).ready(function() {
+        AOSinit();
+        footerReposition();
+    });
 });
