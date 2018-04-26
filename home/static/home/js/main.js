427aaaf6f9290670b2ef4a7579ecab86776aecd8
nctl144 <nctl144@gmail.com>
2018-04-26 15:46:05 -0400

    rollback the footer

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -3,23 +3,7 @@ jQuery(function($) {
         AOS.init();
     }
 
-    function footerReposition() {
-        if (!$(".mdl-grid.result-area").length || $('.mdl-grid.result-area:empty')) {
-            $('.mdl-mini-footer').css({
-                'position': 'absolute',
-                'bottom': '0',
-                'left': '0',
-                'right': '0'
-            });
-        }
-
-        $(".mdl-mini-footer").css({
-            'opacity': '1',
-        });
-    }
-
     $(document).ready(function() {
         AOSinit();
-        footerReposition();
     });
 });
