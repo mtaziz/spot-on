489fa77a2afca6c5c9bc266af61c06d6ae0129d1
nctl144 <nctl144@gmail.com>
2018-04-26 15:33:17 -0400

    style the footer

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -1,3 +1,12 @@
 jQuery(function($) {
     AOS.init();
+
+    if (!$(".mdl-grid.result-area").length || $('.mdl-grid.result-area:empty')) {
+        $('.mdl-mini-footer').css({
+            'position': 'absolute',
+            'bottom': '0',
+            'left': '0',
+            'right': '0'
+        });
+    }
 });
