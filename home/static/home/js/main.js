7135c83ce4541f03bfadf7c4b35644e9ca694a54
nctl144 <nctl144@gmail.com>
2018-05-14 18:56:09 -0400

    fix the js file!

--- home/static/home/js/main.js
+++ home/static/home/js/main.js
@@ -17,14 +17,6 @@ jQuery(function($) {
         });
     }
 
-    var input = document.getElementById("myInput");
-    input.addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.keyCode === 13) {
-            document.getElementById("myBtn").click();
-        }
-    });
-
     function scrollPage() {
         $('#fullpage').fullpage();
     }
