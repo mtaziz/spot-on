50f42c7b104d66aa7137fef4b0c81333fc08f4e7
nctl144 <nctl144@gmail.com>
2018-05-15 12:41:55 -0400

    add login, logout

--- spoton/urls.py
+++ spoton/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.login, {'template_name': 'home/login.html'}, name='login'),
+    path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
 ]
